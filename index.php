22ef032e7bafc4064988a1d37fdc376e101e6a2c
Bob Strecansky <bob.strecansky@gmail.com>
2015-06-06 07:01:00 +0000

    working MYSQL pull

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
 <?php
-include 'header.inc.php';
-echo $_SERVER['HTTP_HOST'];
 
+require 'connect.inc.php';
+
+echo "<h1>Test</h1>This is my page<br><br>";
+
+//include 'header.inc.php';
+//echo $_SERVER['HTTP_HOST'];
+
+$redirect_page = 'https://www.reddit.com/';
+$redirect = 'false';
+
+if ($redirect===true) {
+	header('Location: '.$redirect_page);
+}
+
+$query = "SELECT * from newtable";
+$query_run = mysql_query($query) or die("NOPE");
+
+echo "<table>";
+while($row = mysql_fetch_assoc($query_run)) {
+echo "<tr>";
+	$name =  $row['name'];
+	$age  =  $row['age'];
+	echo"<td>$name</td><td>$age</td>";
+echo "</tr>";
+}
+echo "</table>";
 ?>
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
-</html>
+
---
+++ index3.php
@@ -0,0 +1,14 @@
+<?php ob_start(); ?>
+
+<h1>Test</h1>
+
+<?php
+//include 'header.inc.php';
+//echo $_SERVER['HTTP_HOST'];
+?>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+</html>
+
